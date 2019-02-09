ad301453ff281cc7401242eff0fc7fa61510113a
Patrick Rich <patrick@marathonhomecare.com>
2019-02-09 07:09:03 -0500

    fixed São Paolo

--- Tourist.py
+++ Tourist.py
@@ -1,4 +1,4 @@
-destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "So Paulo, Brazil", "Cairo, Egypt"]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
